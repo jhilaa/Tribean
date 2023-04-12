8004806e6948442fc3ea8849da2d20dd62df7ce9
Julien Hilaire <julien.hilaire@imelmi.net>
2023-04-12 15:18:17 +0200

    Init bootstrap

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,6 +7,8 @@ import {AddUser} from './AddUser';
 import {Login} from './Login';
 import {Header} from './Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
   <div>
--- frontend/my-app/src/Header.js
+++ frontend/my-app/src/Header.js
@@ -1,8 +1,27 @@
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function Header() {
-    return <div>
-        <Link to="listResources">Resources</Link>
-        <Link to="MyResources">Mes resources</Link>
-    </div>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/MyResources">Mes ressources</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/ListResources">Ressources disponibles</Link>
+                    </li>
+                </ul>
+                <div>Bienvenue, ...</div>
+                <button variant="secondary">Se déconnecter</button>
+            </div>
+        </nav>
+    )
 }
