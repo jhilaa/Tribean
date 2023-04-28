0cd0fa5d4d71feaf8850ec5f92d4d4df45f5bfbe
Julien Hilaire <julien.hilaire@imelmi.net>
2023-04-28 11:58:42 +0200

    reactDOM.render vers createRoot

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {createRoot} from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-
+/*
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
@@ -11,5 +11,16 @@ ReactDOM.render(
       </BrowserRouter>
   </React.StrictMode>,
     document.getElementById('root')
+ */
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
